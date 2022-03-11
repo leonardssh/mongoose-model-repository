ef9955c84f3a3ee2afcb53d7c6f11e9c2c6c62f7
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2022-03-11 17:51:57 +0100

    feat: update the interface for reporitory

--- src/types/IRepository.ts
+++ src/types/IRepository.ts
@@ -1,15 +1,30 @@
 import { FilterQuery, UpdateQuery } from 'mongoose';
+import {UpdateResult, DeleteResult} from 'mongodb'
 import { QueryOptions } from './others';
 
 interface IRepository<T> {
+  //create
   create(data: T | T[]): Promise<T | void | T[]>;
+
+  //query
   findById(id: string, options?:QueryOptions): Promise<T | null>;
   findOne(filter: FilterQuery<T>, option?:QueryOptions): Promise<T | null>;
-  findByIdAndDelete(id: string): Promise<void>;
-  deleteOne(filter: FilterQuery<T>): Promise<void>;
-  deleteMany()
-  findAndUpdate(filter: FilterQuery<T>, update: UpdateQuery<T>): Promise<void>;
-  find(filter: FilterQuery<T>, select?: string, sort?: any, options?: { lean?: boolean }): Promise<T[]>;
+  find(filter: FilterQuery<T>, options?:QueryOptions): Promise<T[]>;
+  countDocuments(filter: FilterQuery<T>): Promise<number>
+
+  //deletion
+  findByIdAndDelete(id: string): Promise<T | null>;
+  findOneAndDelete(filter: FilterQuery<T>):Promise<T | null>;
+  deleteOne(filter: FilterQuery<T>): Promise<DeleteResult>;
+  deleteMany(filter: FilterQuery<T>): Promise<DeleteResult>;
+
+  //update
+  updateMany(filter: FilterQuery<T>, update: UpdateQuery<T>): Promise<UpdateResult>;
+  updateOne(filter: FilterQuery<T>,update: UpdateQuery<T>): Promise<UpdateResult>;
+  findOneAndUpdate(filter: FilterQuery<T>,update: UpdateQuery<T>): Promise<T | null>;
+  findByIdAndUpdate(filter: FilterQuery<T>,update: UpdateQuery<T>): Promise<T | null>;
 }
+
+
 
 export default IRepository;
--- src/types/Repository.ts
+++ src/types/Repository.ts
@@ -1,66 +1,55 @@
-// import IRepository from './IRepository';
-// import { FilterQuery, UpdateQuery, Model } from 'mongoose';
+import IRepository from './IRepository';
+import { FilterQuery, UpdateQuery, Model } from 'mongoose';
+import { DeleteResult, UpdateResult } from 'mongodb';
+import { QueryOptions } from './others';
 
-// class Repository<T> implements IRepository<T> {
-//   model: Model<T>;
-//   constructor(model: Model<T>) {
-//     this.model = model;
-//   }
+abstract class Repository<T> implements IRepository<T> {
+  model: Model<T>;
+  constructor(model: Model<T>) {
+    this.model = model;
+   
+  }
+  create(data: T | T[]): Promise<void | T | T[]> {
+    throw new Error('Method not implemented.');
+  }
+  findById(id: string, options?: QueryOptions): Promise<T | null> {
+    throw new Error('Method not implemented.');
+  }
+  findOne(filter: FilterQuery<T>, option?: QueryOptions): Promise<T | null> {
+    throw new Error('Method not implemented.');
+  }
+  find(filter: FilterQuery<T>, options?: QueryOptions): Promise<T[]> {
+    throw new Error('Method not implemented.');
+  }
+  countDocuments(filter: FilterQuery<T>): Promise<number> {
+    throw new Error('Method not implemented.');
+  }
+  findByIdAndDelete(id: string): Promise<T | null> {
+    throw new Error('Method not implemented.');
+  }
+  findOneAndDelete(filter: FilterQuery<T>): Promise<T | null> {
+    throw new Error('Method not implemented.');
+  }
+  deleteOne(filter: FilterQuery<T>): Promise<DeleteResult> {
+    throw new Error('Method not implemented.');
+  }
+  deleteMany(filter: FilterQuery<T>): Promise<DeleteResult> {
+    throw new Error('Method not implemented.');
+  }
+  updateMany(filter: FilterQuery<T>, update: UpdateQuery<T>): Promise<UpdateResult> {
+    throw new Error('Method not implemented.');
+  }
+  updateOne(filter: FilterQuery<T>, update: UpdateQuery<T>): Promise<UpdateResult> {
+    throw new Error('Method not implemented.');
+  }
+  findOneAndUpdate(filter: FilterQuery<T>, update: UpdateQuery<T>): Promise<T | null> {
+    throw new Error('Method not implemented.');
+  }
+  findByIdAndUpdate(filter: FilterQuery<T>, update: UpdateQuery<T>): Promise<T | null> {
+    throw new Error('Method not implemented.');
+  }
 
-//   async create(data: T | T[]): Promise<void | T | T[]> {
-//     const result = await this.model.create(data);
-//     return result;
-//   }
+  
+}
 
-//   async findById(id: string, select?: string, options?: { lean: boolean }): Promise<T | null> {
-//     let query = this.model.findById(id);
-//     if (select) query = query.select(select);
-
-//     if (options) {
-//       const { lean } = options;
-//       if (lean) query = query.lean();
-//     }
-//     const data = await query;
-
-//     return data;
-//   }
-
-//   async findByIdAndDelete(id: string): Promise<void> {
-//     throw new Error('Method not implemented.');
-//   }
-
-//   async findAndDelete(filter: FilterQuery<T>): Promise<void> {
-//     throw new Error('Method not implemented.');
-//   }
-
-//   async findOne(filter: FilterQuery<T>, select?: string, options?: { lean?: boolean }): Promise<T | null> {
-//     let query = this.model.findOne(filter);
-//     if (select) query = query.select(select);
-
-//     if (options) {
-//       const { lean } = options;
-//       if (lean) query = query.lean();
-//     }
-//     const data = await query;
-//     return data;
-//   }
-
-//   async findAndUpdate(filter: FilterQuery<T>, update: UpdateQuery<T>): Promise<void> {
-//     throw new Error('Method not implemented.');
-//   }
-
-//   async find(filter: FilterQuery<T>, select?: string, sort?: any, options?: { lean?: boolean }): Promise<T[]> {
-//     let query = this.model.find(filter);
-//     if (select) query = query.select(select);
-//     if (sort) query = query.sort(sort);
-//     if (options) {
-//       const { lean } = options;
-//       if (lean) query = query.lean();
-//     }
-
-//     const data = await query;
-//     return data;
-//   }
-// }
-
-// export default Repository;
+export default Repository;
