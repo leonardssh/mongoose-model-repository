77257d4af8aef9d19ad4ad768041b1d091504ac2
OSEMUDIAMEN ITUA <osemudiamenitua@gmail.com>
2022-03-12 18:25:06 +0100

    export new type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import IRepository from './types/IRepository';
 import Repository from './types/Repository';
-import { QueryOptions, PaginatedResult, UpdateOptions } from './types/others';
+import { QueryOptions, PaginatedResult, UpdateOptions, QueryOptionsExtended } from './types/others';
 
-export { IRepository, QueryOptions, UpdateOptions, PaginatedResult };
+export { IRepository, QueryOptions, UpdateOptions, PaginatedResult, QueryOptionsExtended };
 export default Repository;
